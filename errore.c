1ba0809130224a1f4740caedf45c4336662e7e63
CAridorc <riki100024@gmail.com>
2018-11-24 17:12:15 +0100

    piu cifre

--- errore.c
+++ errore.c
@@ -5,8 +5,9 @@
 
 double simple_oscill_energy(double x, double v, double w2);
 
-int main(){
+int main(int argc, char* argv[]) {
     // Read parameters from input file
+    //puts("hi");
     FILE *input = fopen("input/input_errore.dat", "r");
     FILE *output = fopen("data/energia_errore_vs_dt.dat", "w");
     double x0, v0, dt, tmax;
@@ -29,10 +30,12 @@ int main(){
     t_state end;
     double E0 = simple_oscill_energy(x0, v0, params[1]);
     double E;
-    for (; dt > 0.0001; dt/=1.2) {
-        end = simulate(x0, v0, dt, tmax, params, method);
+    for (; dt > 0.0001; dt/=1.1) {
+			//puts("hi");
+        end = simulate(x0, v0, dt, tmax+dt, params, method);
+        //puts("hih");
         E = simple_oscill_energy(end.x, end.v, params[1]);
-        fprintf(output, "%lf %lf\n", fabs(E-E0)/E0, dt);
+        fprintf(output, "%.12lf %.12lf\n", fabs(E-E0)/E0, dt);
     }
     return 0;
 }
