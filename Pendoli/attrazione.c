c08c5b21af0e021ff200e8e91d7e1b7c828e10cb
CAridorc <riki100024@gmail.com>
2018-12-11 18:58:07 +0100

    Add files via upload

--- Pendoli/attrazione.c
+++ Pendoli/attrazione.c
@@ -19,7 +19,7 @@ int main(){
     double TF;
     fscanf(input, "%lf %lf", &dt, &tmax);
     fscanf(input, "%d", &method);
-    fscanf(input, "%lf %lf %lf %lf %lf", &params[0], &params[1], &params[2], &params[3], &TF);
+    fscanf(input, "%lf %lf %lf %lf", &params[1], &params[2], &params[3], &TF);
     /* PARAMS:
      * params[0] = phicode
      * params[1] = w2
@@ -27,36 +27,13 @@ int main(){
      * params[3] = a0
      * params[4] = WF
      */
+     params[0] = 1; // L'effetto caotico si manifesta solo con il pendolo
 
-    t_state (*integrate)(t_state state, double dt, double params[]);
-    switch (method){
-        case 0:
-            integrate = Eulero;
-            break;
-        case 1:
-            integrate = Eulero_Cromer;
-            break;
-        case 2:
-            integrate = RK2;
-            break;
-        case 3:
-            integrate = RK4;
-            break;
-        default:
-            printf("Errore: metodo %d non riconosciuto\n", method);
-            exit(1);
-    }
-
-  t_state state;
+  t_state end;  
   for (x0=-M_PI; x0<M_PI; x0+=M_PI/500) {
 		for (v0=-M_PI; v0<M_PI; v0+=M_PI/500) {
-			state.x = x0;
-			state.v = v0;
-			state.t = 0;
-			while(state.t < tmax) {
-				state = integrate(state, dt, params);
-			}
-			fprintf(output, "%lf %lf %lf\n", x0, v0, state.v);
+			end = simulate(x0, v0, dt, tmax, params, method);
+			fprintf(output, "%lf %lf %lf\n", x0, v0, end.v);
 		}			
 	}
 	
